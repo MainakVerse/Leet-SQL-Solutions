1ce2ca595e547fc5a65506b1f4dd3a756173a5a8
Mainak Chaudhuri <mainakchaudhuri671@gmail.com>
2025-11-21 07:22:18 +0530

    Create Find-Customer-Referee.sql

---
+++ Find-Customer-Referee.sql
@@ -0,0 +1,31 @@
+'''Find the names of the customer that are either:
+
+referred by any customer with id != 2.
+not referred by any customer.
+Return the result table in any order.
+
+  Input: 
+Customer table:
++----+------+------------+
+| id | name | referee_id |
++----+------+------------+
+| 1  | Will | null       |
+| 2  | Jane | null       |
+| 3  | Alex | 2          |
+| 4  | Bill | null       |
+| 5  | Zack | 1          |
+| 6  | Mark | 2          |
++----+------+------------+
+Output: 
++------+
+| name |
++------+
+| Will |
+| Jane |
+| Bill |
+| Zack |
++------+
+  
+  '''
+
+select name from Customer where referee_id != 2 or referee_id is Null
